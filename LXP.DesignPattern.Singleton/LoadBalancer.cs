0903cebf98b215fc5285534b58f662eb6453cde8
LiuXiaoPeng <liuxiaopeng@mediinfo.com.cn>
2021-02-13 23:55:13 +0800

    单例模式负载均衡器示例代码

---
+++ LXP.DesignPattern.Singleton/LoadBalancer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LXP.DesignPattern.Singleton
+{
+    /// <summary>
+    /// 负载均衡器：单例类，真实环境可能非常复杂，这里只列出部分与模式相关的代码
+    /// </summary>
+    public class LoadBalancer
+    {
+        //私有静态成员变量，保存唯一实例
+        private static LoadBalancer loadBalancer = null;
+        //服务器集合
+        private List<string> serverList = null;
+
+        /// <summary>
+        /// 私有构造函数
+        /// </summary>
+        private LoadBalancer()
+        {
+            serverList = new List<string>();
+        }
+
+        /// <summary>
+        /// 公有静态成员方法，返回唯一实例
+        /// </summary>
+        /// <returns></returns>
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (loadBalancer == null)
+                loadBalancer = new LoadBalancer();
+            return loadBalancer;
+        }
+
+        //增加服务器
+        public void AddServer(string server)
+        {
+            serverList.Add(server);
+        }
+
+        //删除服务器
+        public void RemoveServer(string server)
+        {
+            serverList.Remove(server);
+        }
+
+        //使用Random类随机获取服务器
+        public string GetServer()
+        {
+            var random = new Random();
+            var i = random.Next(serverList.Count);
+            return serverList[i];
+        }
+
+    }
+}
---
+++ LXP.DesignPattern.Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LXP.DesignPattern.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建4个LoadBalancer对象
+            LoadBalancer balancer1, balancer2, balancer3, balancer4;
+            balancer1 = LoadBalancer.GetLoadBalancer();
+            balancer2 = LoadBalancer.GetLoadBalancer();
+            balancer3 = LoadBalancer.GetLoadBalancer();
+            balancer4 = LoadBalancer.GetLoadBalancer();
+
+            //判断服务器负载均衡器是否相同
+            if (balancer1 == balancer2 && balancer2 == balancer3 && balancer3 == balancer4)
+            {
+                Console.WriteLine("服务器负载均衡器具有唯一性！");
+            }
+
+            //增加服务器
+            balancer1.AddServer("server 1");
+            balancer1.AddServer("server 2");
+            balancer1.AddServer("server 3");
+            balancer1.AddServer("server 4");
+
+            for (int i = 0; i < 10; i++)
+            {
+                var server = balancer1.GetServer();
+                Console.WriteLine("分发请求至服务器：" + server);
+            }
+        }
+    }
+}
